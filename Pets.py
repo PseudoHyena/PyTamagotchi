e645c15a981b0d1f331b2605a3d143bfbb8515f8
Artem Bagnyuk <anzen.bagnyuk@gmail.com>
2017-10-30 18:08:28 +0300

    Add Pet class

--- Pets.py
+++ Pets.py
@@ -10,3 +10,12 @@ class Goose(GameObject):
         self.y = posY
         self._width = images[0].get_width()
         self._height = images[0].get_height()
+
+class Pet(GameObject):
+    def __init__(self, image):
+        GameObject.__init__(self)
+        self._object = image
+        self.x = 0
+        self.y = 0
+        self._width = image.get_width()
+        self._height = image.get_height()
